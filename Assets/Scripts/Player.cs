fc4e97c8af7379281cf0b14b4ac3b9257e2c0c06
tayutaedomo <tayutaedomo@gmail.com>
2018-02-14 01:09:56 +0900

    Complete moving-the-player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxisRaw ("Horizontal");
+		float y = Input.GetAxisRaw ("Vertical");
+		Debug.Log ("x:y, " + x + " " + y);
+
+		Vector2 direction = new Vector2 (x, y).normalized;
+		GetComponent<Rigidbody2D> ().velocity = direction * speed;
+	}
+}
