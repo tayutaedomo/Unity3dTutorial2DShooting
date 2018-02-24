49fab8d923f9a49a71ca9a5b1bdf5a6a758e6f78
tayutaedomo <tayutaedomo@gmail.com>
2018-02-24 14:53:06 +0900

    Complete spawning-waves

---
+++ Assets/Scripts/Emitter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emitter : MonoBehaviour {
+
+	public GameObject[] waves;
+
+	private int currentWate;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		if (waves.Length == 0) {
+			yield break;
+		}
+	
+		while (true) {
+			GameObject wave = (GameObject)Instantiate (waves [currentWate], transform.position, Quaternion.identity);
+
+			wave.transform.parent = transform;
+
+			while (wave.transform.childCount != 0) {
+				yield return new WaitForEndOfFrame ();
+			}
+
+			Destroy (wave);
+
+			if (waves.Length <= ++currentWate) {
+				currentWate = 0;
+			}
+		}
+	}
+}
